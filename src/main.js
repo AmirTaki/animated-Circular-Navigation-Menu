59f5e11efddfc72badf5087285fefb082c872cd6
amir <amirtaki623@gmail.com>
2025-08-12 22:52:56 +0330

    js part 2

--- src/main.js
+++ src/main.js
@@ -1,2 +1,6 @@
 const toggle = document.querySelector(".toggle");
 const menu = document.querySelector(".menu");
+
+toggle.addEventListener('click', (e) => {
+    menu.classList.toggle("active")
+})
